df51df76fb8e23586f7f2745cd7cd36c1d52918e
Mike Bishop <101715089+mikebishop0@users.noreply.github.com>
2024-07-01 18:21:56 +0100

    Add files via upload

---
+++ Guess a Word Game/js/words.js
@@ -0,0 +1,12 @@
+const wordList = [
+  { word: "python", hint: "programming language" },
+  { word: "guitar", hint: "musical instrument" },
+  { word: "aim", hint: "purpose or intention" },
+  { word: "venus", hint: "planet in our solar system" },
+  { word: "gold", hint: "precious metal" },
+  { word: "ebay", hint: "online shopping site" },
+  { word: "golang", hint: "programming language" },
+  { word: "coding", hint: "related to programming" },
+  { word: "matrix", hint: "sci-fi movie" },
+  { word: "bugs", hint: "programming issues" },
+];
